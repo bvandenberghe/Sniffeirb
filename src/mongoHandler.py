702e8279f3ec0e2e7505341b120170f0e61872f5
nverdier42 <nverdier42@gmail.com>
2012-11-12 12:55:06 +0100

    ecriture dans mongodb

---
+++ src/mongoHandler.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from pymongo import Connection
+from JsonDisplayHandler import getProtocol
+from scapy.all import *
+from sniffeirb_globals import *
+
+
+def insertPacket(pkt):
+	global db;
+	proto=getProtocol(pkt)
+	Type="unknown"
+	if(IP in pkt):
+		Type="IP"
+		if(proto=="TCP" or proto =="UDP"):
+			dport=pkt.dport
+			sport=pkt.sport
+		else:
+			dport=""
+			sport=""
+		spec = {"src" : pkt[IP].src, "dst" : pkt[IP].dst, "dport" : dport, "sport" : sport, "proto" : proto , "type" : Type}
+		if(proto=="TCP"):
+			db.stream.update(spec, { "$push" : {"packets" : { "flags" : "TODO", "ts" : pkt.time, "seq" : pkt.seq, "ack" : pkt.ack, "data" : pkt.sprintf("%Raw%") }}},upsert=True)
+		elif(proto=="UDP"):
+			db.stream.update(spec, { "$push" : {"packets" : { "flags" : "TODO", "ts" : pkt.time, "data" : pkt.sprintf("%Raw%") }}},upsert=True)
+		#for p in db.stream.find():
+		#	print p
