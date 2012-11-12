8b09a88284bf23f63630eae201e354bf64ace7f6
nretrain <nicolas.retrain@gmail.com>
2012-11-12 16:31:06 +0100

    modification de la fonction de reassebmlage

--- src/reassemble.py
+++ src/reassemble.py
@@ -5,29 +5,49 @@ from threading import Thread
 import sys, os
 from scapy.all import *
 import array
-import globals
 import sys
+from lianatree import *
+from connect import connectMongo
 
-#get Packets from specified IP and reassemble pages
-class ReassembleThread(Thread):
-	def __init__ (self,filter):
-		Thread.__init__(self)
-		self.filter = filter
-	def run(self):
-		try:
-			reassemble(IPdest)
-		except KeyboardInterrupt:
-			print "reassemble thread exited ..."
-			exit(0)
-	#def callback(self,pkt):
-		#sniff_buffer.append(pkt)#'%TCP.payload%'
-	def reassemble (src, dst, sport, dport):
+
+
+def reassemble_stream (src, dst, sport, dport):
 	#start reassembling stream from datastream
-		#connection to DB
-		connection=Connection('localhost', 27017)
-		db=connection['test']
-		#get data from column stream for specified fields
-		flow=stream.find_one({"type": "IP"}, {"src": src}, {"dst": dst}, {"sport": sport}, {"dport": dport})
-		#sort by IP sequence number /!\ issue a warning if twice same ip sequence number and makes 2 != streams
-		sorted(flow.packets, key=lambda packet: packet.seq)
-		print(flow.packets)
+	#connection to DB
+	db = connectMongo('idsession')
+	#get data from column stream for specified fields
+	flow=db.stream.find_one({"proto": "TCP", "src": src, "dst": dst, "sport": sport, "dport": dport})
+	#sort by IP sequence number /!\ issue a warning if twice same ip sequence number and makes 2 != streams
+	#packets=sorted(flow.packets, key=lambda packet: packet.seq)
+	#print(packets)
+	#print flow
+	liana=LianaTree()
+	for pkt in flow['packets']:
+		liana.feed(pkt)
+	#on reassemble en dedoublant pour chaque next double
+	#cheetah's algorithm
+	flow_tab = []
+	data = [{'data': liana.packets[x]['data'], 'initseq': liana.packets[x]['seq'], 'nextseq': liana.packets[x]['nextseq']} for x in liana.packets if not liana.packets[x]['prev']]
+	print data
+	for d in data:
+		test_bool = False
+		for x in liana.packets:
+			if d['nextseq']==liana.packets[x]['seq']:
+				d['data']=d['data']+(liana.packets[x]['data'])
+				d['nextseq']+=len(liana.packets[x]['data'])
+				test_bool = True
+				print "tour"+ str(x)
+		if test_bool==False:
+			break
+	for d in data:
+		print d
+		print "###################################################################################################################################################\n"
+
+# on ne supprime pas les données du liana tree ... quand tous les nextseq pointent sur des trucs vides : ceci est la condition d'arret 
+# en debut de boucle, on place le flag à false
+# si on fait quelquechose pendant une boucle on met le flag a vrai   
+# sinon, à la fin de la boucle si le flag vaut false, on s'arrête		
+		
+
+reassemble_stream('147.210.183.243', '172.20.0.10', 3128, 36096)
+
