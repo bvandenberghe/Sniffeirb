5230dc9255b41de17fee709dc8d1695c448b99aa
nverdier42 <nverdier42@gmail.com>
2012-11-16 15:00:02 +0100

    modif main

--- src/connect.py
+++ src/connect.py
@@ -1,7 +1,10 @@
 from pymongo import Connection
 
 def connectMongo(database):
-	global db
-	connection = Connection('localhost', 27017)
-	db = connection[database]
+	try:
+		connection = Connection('localhost', 27017)
+		db = connection[database]
+	except:
+		print "error: can't connect to the mongo database"
+		exit(1)
 	return db
